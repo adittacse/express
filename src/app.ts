40844c7e31239e966cd3afdb0c99d388e56407a0
Aditta Chakraborty <cseaditta@gmail.com>
2026-06-06 03:00:22 +0600

    project folder updated to moduler system

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { type Application, type Request, type Response } from "express";
+import { pool } from "./db";
+import { userRoute } from "./modules/user/user.route";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+    // res.send("Learning ExpressJS with TypeScript");
+    return res.status(200).json({
+        message: "Learning ExpressJS with TypeScript",
+        author: "Next Level Web Development",
+    });
+});
+
+app.use("/api/users", userRoute);
+
+export default app;
---
+++ src/modules/user/user.route.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { userController } from "./user.controller";
+
+const router = Router();
+
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getSingleUser);
+router.post("/", userController.createUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+
+export const userRoute = router;
